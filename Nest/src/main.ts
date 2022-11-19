04c6235e26ca4b07ca5948d2bffcec9478c2e240
SergiyRybin <serhiobax@gmail.com>
2022-11-19 11:14:22 +0200

    added nest

--- Nest/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
-async function bootstrap() {
-  const PORT = 3000;
-  const app = await NestFactory.create(AppModule);
-  await app.listen(PORT, () => console.log(`Server started at ${PORT} port`));
-}
-bootstrap();
