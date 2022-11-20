29e1c663a7b656729635a895abc20cb59e963040
SergiyRybin <serhiobax@gmail.com>
2022-11-20 12:10:47 +0200

    added nest conf

--- Nest/src/app.module.ts
+++ Nest/src/app.module.ts
@@ -1,8 +1,25 @@
 import { Module } from '@nestjs/common';
-import { CustomersModule } from './customers/customers.module';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [CustomersModule],
   controllers: [],
   providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.PSQL_HOST,
+      port: Number(process.env.PSQL_PORT),
+      username: process.env.PSQL_USESR,
+      password: process.env.PSQL_PASS,
+      database: process.env.PSQL_DB,
+      models: [],
+      autoLoadModels: true,
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}
